11ab756819120e2cc1b7a50a469a89b36ca83825
Rati3010 <mratikanta1997@gmail.com>
2024-02-28 08:11:13 +0530

    created a test api route

--- server/index.js
+++ server/index.js
@@ -1,15 +1,22 @@
-import express from "express";
-import dotenv from "dotenv";
-import connection from "./config/db.js";
+import express from 'express';
+import dotenv from 'dotenv';
+import connection from './config/db.js';
+import userRoutes from './routes/user.route.js';
 
 dotenv.config();
 const app = express();
+app.use(express.json());
 
-app.listen(process.env.PORT||8080, async()=>{
-    try {
-        await connection;
-        console.log("Successfully established the connection with DB");
-    } catch (error) {
-        console.log(error);
-    }
+app.get('/test', (req, res) => {
+  res.json({ message: 'Api is working' });
+});
+app.use('/api/user',userRoutes);
+
+app.listen(process.env.PORT || 8080, async () => {
+  try {
+    await connection;
+    console.log('Successfully established the connection with DB');
+  } catch (error) {
+    console.log(error);
+  }
 });
