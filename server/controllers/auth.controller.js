fb6e0f1a1c3516e7a1e9e90443290c19134720ff
Rati3010 <mratikanta1997@gmail.com>
2024-02-29 15:58:17 +0530

    created signin api route

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -1,4 +1,5 @@
 import bcryptjs from 'bcryptjs';
+import jwt from 'jsonwebtoken';
 import User from '../models/user.model.js';
 import { errorHandler } from '../utils/error.js';
 
@@ -19,6 +20,36 @@ export const signup = async (req, res, next) => {
   try {
     await newUser.save();
     res.json({ message: 'Signup' });
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const signin = async (req, res, next) => {
+  const { username, password } = req.body;
+  if (!username || !password) {
+    return next(errorHandler(400, 'All fields are required'));
+  }
+  try {
+    const validUser = await User.findOne({ username });
+    if (!validUser) {
+      return next(errorHandler(404, 'Wrong Credentials'));
+    }
+    const validPassword = bcryptjs.compareSync(password, validUser.password);
+    if (!validPassword) {
+     return next(errorHandler(400, 'Invalid Password'));
+    }
+    const token = jwt.sign(
+      {
+        id: validUser._id,
+      },
+      process.env.JWT_SECRET
+    );
+    const { password: pass, ...rest } = validUser._doc;
+    res
+      .status(200)
+      .cookie('access_token', token, { httpOnly: true })
+      .json(rest);
   } catch (error) {
     next(error);
   }
