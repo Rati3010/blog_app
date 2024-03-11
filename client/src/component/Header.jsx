68403a5850ee3f67b56c956daf7114fa447e33a6
Rati3010 <mratikanta1997@gmail.com>
2024-03-11 07:37:17 +0530

    completed the dark theme task

--- client/src/component/Header.jsx
+++ client/src/component/Header.jsx
@@ -2,12 +2,19 @@ import React from 'react';
 import { Avatar, Button, Dropdown, Navbar, TextInput } from 'flowbite-react';
 import { Link, useLocation } from 'react-router-dom';
 import { AiOutlineSearch } from 'react-icons/ai';
-import { FaMoon } from 'react-icons/fa';
-import { useSelector } from 'react-redux';
+import { FaMoon, FaSun } from 'react-icons/fa';
+import { useSelector,useDispatch } from 'react-redux';
+import { toogleTheme } from '../redux/theme/themeSlice';
 
 const Header = () => {
   const path = useLocation().pathname;
   const {currentUser} = useSelector(state => state.user);
+  const {theme} = useSelector(state => state.theme);
+  const dispatch = useDispatch();
+  const handleTheme = () =>{
+    console.log('changed the theme');
+      dispatch(toogleTheme());
+  }
   return (
     <Navbar className="border-b-2">
       <Link
@@ -31,8 +38,8 @@ const Header = () => {
         <AiOutlineSearch />
       </Button>
       <div className="flex gap-2 md:order-2">
-        <Button className=" w-12 h-10 hidden sm:inline" color="gray" pill>
-          <FaMoon />
+        <Button className=" w-12 h-10 hidden sm:inline" color="gray" pill onClick={handleTheme}>
+          {theme === 'light' ? <FaMoon/> :<FaSun/>}
         </Button>
         {
           currentUser ? <Dropdown arrowIcon={false} inline label={<Avatar alt ='user' img={currentUser.profilePicture} rounded/>}>
